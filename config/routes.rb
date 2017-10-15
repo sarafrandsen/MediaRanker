b10289ea6f82fe58a33a36e97c3861bf1968a464
Sara Frandsen <sara.frandsen@gmail.com>
2017-10-15 13:59:07 -0700

    basic page layout main

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root 'works#main'
+  root 'main#index'
 
   resources :users, only: [:index, :new, :create, :show]
   resources :votes, only: [:index, :new, :create]
