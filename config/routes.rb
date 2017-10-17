d9969a7b6ce7e0fa25b6e830a93a5e1853b2efa2
Sara Frandsen <sara.frandsen@gmail.com>
2017-10-16 20:11:46 -0700

    add basic routes using resources: users sessions works

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'main#index'
+
+  resources :users
+  resources :sessions
+  resources :works
 end
