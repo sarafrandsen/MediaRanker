244d530195030ee3cc6e0bf8f9ac0b7b2cc6504d
Sara Frandsen <sara.frandsen@gmail.com>
2017-10-16 20:18:38 -0700

    add empty methods in works ctrl

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,2 +1,43 @@
 class WorksController < ApplicationController
+
+  def root
+    # homepage
+  end
+
+  def index
+    # view all works
+  end
+
+  def new
+    @work = Work.new
+  end
+
+  def create
+    # POST new work
+  end
+
+  def show
+    # individual work
+  end
+
+  def edit
+    # GET changes
+  end
+
+  def update
+    # POST changes
+  end
+
+  def destroy
+    # DELETE
+  end
+
+  def upvote
+    # vote on works
+  end
+
+  private
+  def work_params
+    params.require(:work).permit(:title, :category, :creator, :description, :publication_year)
+  end
 end
