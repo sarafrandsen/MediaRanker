1d8b3da76943a10692729297a8038629e202e895
Sara Frandsen <sara.frandsen@gmail.com>
2017-10-16 20:06:13 -0700

    create users votes works models, users sessions works ctrls

---
+++ db/migrate/20171017025439_create_votes.rb
@@ -0,0 +1,10 @@
+class CreateVotes < ActiveRecord::Migration[5.1]
+  def change
+    create_table :votes do |t|
+      t.belongs_to :user, index: true
+      t.belongs_to :piece, index: true
+      
+      t.timestamps
+    end
+  end
+end
---
+++ db/migrate/20171017025446_create_works.rb
@@ -0,0 +1,13 @@
+class CreateWorks < ActiveRecord::Migration[5.1]
+  def change
+    create_table :works do |t|
+      t.string :title
+      t.string :creator
+      t.string :description
+      t.date :publication_date
+      t.string :category
+      
+      t.timestamps
+    end
+  end
+end
