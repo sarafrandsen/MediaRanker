628bb14c54431626b4e93751860eb2b8e985de36
Sara Frandsen <sara.frandsen@gmail.com>
2017-10-16 22:54:04 -0700

    add sessions routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,8 @@ Rails.application.routes.draw do
   resources :users
   resources :sessions
   resources :works
+  
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
 end
