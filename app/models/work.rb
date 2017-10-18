1ddad477dcec8599622dd8f4adff35c9d64aa3d9
Sara Frandsen <sara.frandsen@gmail.com>
2017-10-17 21:58:03 -0700

    add top ten method to works and a counter cache

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,6 +1,10 @@
 class Work < ApplicationRecord
   has_many :votes
 
+  def self.top_ten(category)
+    return Work.where(category: category).order(votes_count: :desc).take(10)
+  end
+
   def get_upvotes
     return Vote.where(work: self)
   end
