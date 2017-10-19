10537d210e78bac6cda707771ebea9d45e55c264
Sara Frandsen <sara.frandsen@gmail.com>
2017-10-19 13:07:54 -0700

    push

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find_by(id: params[:id])
+    @user = User.find(params[:id])
     unless @user
       render not_found
     end
